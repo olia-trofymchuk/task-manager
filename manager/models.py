8003065bc0ecf38d746cb0dd597a9f0c33340ffe
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-11-10 11:36:50 +0200

    Create model TaskType

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
