1c0b532398ec4d93b8e9a5ac07f3548808e07fa0
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-11-10 12:13:44 +0200

    Add string representation to Worker model

--- manager/models.py
+++ manager/models.py
@@ -26,6 +26,9 @@ class Worker(AbstractUser):
         verbose_name = "worker"
         verbose_name_plural = "workers"
 
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
 
 class Task(models.Model):
     CHOICES = (
