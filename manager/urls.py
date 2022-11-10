5fbf1e2cfb991752fd72e14d2d646ccc433a8f85
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-11-10 14:28:47 +0200

    Add views for reading information from database

--- manager/urls.py
+++ manager/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
 
-from manager.views import index
+from manager.views import index, TaskTypeListView, PositionListView, WorkerListView, WorkerDetailView, TaskListView, \
+    TaskDetailView
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path(
+        "task_types/",
+        TaskTypeListView.as_view(),
+        name="task_type_list",
+    ),
+    path("positions/", PositionListView.as_view(), name="position_list"),
+    path("workers/", WorkerListView.as_view(), name="worker_list"),
+    path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker_detail"),
+    path("tasks/", TaskListView.as_view(), name="task_list"),
+    path(
+        "tasks/<int:pk>/", TaskDetailView.as_view(), name="task_detail"
+    ),
 ]
 
 app_name = "manager"
