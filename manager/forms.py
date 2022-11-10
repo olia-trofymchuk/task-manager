603f89ad949d32196fad707dd0d4f1523721718b
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-11-10 22:57:32 +0200

    Add search forms for all models

--- manager/forms.py
+++ manager/forms.py
@@ -1,6 +1,11 @@
-from django.contrib.auth.forms import UserCreationForm
+import datetime
 
-from manager.models import Worker
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+
+from .models import Worker, Task
 
 
 class WorkerCreationForm(UserCreationForm):
@@ -11,3 +16,71 @@ class WorkerCreationForm(UserCreationForm):
             "last_name",
             "position",
         )
+
+
+class DateInput(forms.DateInput):
+    input_type = "date"
+
+
+class TaskForm(forms.ModelForm):
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+
+    class Meta:
+        model = Task
+        fields = ["name", "description", "deadline",
+                  "priority", "task_type", "assignees"]
+        widgets = {
+            "deadline": forms.TextInput(attrs={"type": "date"}),
+        }
+
+    def clean_deadline(self):
+        return validate_deadline(self.cleaned_data["deadline"])
+
+
+class WorkerSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by username..."}),
+    )
+
+
+class TaskSearchForm(forms.Form):
+    task = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by task..."}),
+    )
+
+
+class TaskTypeSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by task type..."}),
+    )
+
+
+class PositionSearchForm(forms.Form):
+    type = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by position..."}),
+    )
+
+
+def validate_deadline(
+    deadline,
+):
+    if deadline < datetime.date.today():
+        raise ValidationError("You should set date for next days")
+
+    return deadline
