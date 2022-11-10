4d850d05604b8f8761e1871b97af8e621219c75a
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-11-10 13:32:27 +0200

    Add view for index page

--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Worker, Task, Position, TaskType
+
+
+@login_required
+def index(request):
+    num_worker = Worker.objects.count()
+    num_tasks = Task.objects.count()
+    num_positions = Position.objects.count()
+    num_task_types = TaskType.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_worker": num_worker,
+        "num_tasks": num_tasks,
+        "num_positions": num_positions,
+        "num_task_types": num_task_types,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "manager/index.html", context=context)
