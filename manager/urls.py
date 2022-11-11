0603a96a368d4405f9d6cf797d529923c26510ca
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-11-11 07:22:40 +0200

    Add urls for task views

--- manager/urls.py
+++ manager/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from manager.views import index, TaskTypeListView, PositionListView, WorkerListView, WorkerDetailView, TaskListView, \
     TaskDetailView, PositionCreateView, PositionUpdateView, PositionDeleteView, TaskTypeCreateView, TaskTypeUpdateView, \
-    TaskTypeDeleteView, WorkerCreateView
+    TaskTypeDeleteView, WorkerCreateView, WorkerDeleteView, TaskCreateView, toggle_assign_to_task, TaskDeleteView, \
+    TaskUpdateView, WorkerUpdateView
 
 urlpatterns = [
     path("", index, name="index"),
@@ -32,10 +33,24 @@ urlpatterns = [
     path("positions/<int:pk>/delete/", PositionDeleteView.as_view(), name="position-delete"),
     path("workers/", WorkerListView.as_view(), name="worker-list"),
     path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
+    path("workers/<int:pk>/update", WorkerUpdateView.as_view(), name="worker-update"),
     path("workers/create/", WorkerCreateView.as_view(), name="worker-create"),
+    path("workers/<int:pk>/delete/", WorkerDeleteView.as_view(), name="worker-delete"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path(
         "tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"
+    ),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path(
+        "tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"
+    ),
+    path(
+        "tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"
+    ),
+    path(
+        "tasks/<int:pk>/toggle-assign/",
+        toggle_assign_to_task,
+        name="toggle-task-assign",
     ),
 ]
 
